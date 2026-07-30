4fa338e809325efb2a03bd06260e3406c391aa9c
Surya <suryaram.r@gmail.com>
2026-07-31 02:56:49 +0530

    Fifth commit

---
+++ Basic5.js
@@ -0,0 +1,14 @@
+function display(){
+    console.log("Hello");
+}
+display();
+
+const display1=function(){
+console.log("Welcome");
+}
+display1();
+
+const add=(a,b)=>{
+    return a+b;
+}
+add(10,20);         
