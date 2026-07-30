881d0d20f16ea2d672537fd33314dfcabbd0ecbe
Surya <suryaram.r@gmail.com>
2026-07-31 02:53:44 +0530

    Third commit

---
+++ Basic3.js
@@ -0,0 +1,21 @@
+let studentname="Surya";
+console.log(typeof studentname);
+let mark=95;
+console.log(typeof mark);
+let ispassed=true;
+console.log(typeof ispassed);
+let age;
+console.log(typeof age);
+let salary=null;
+console.log(typeof salary);
+let students=["Java","JS","Playwright"];
+console.log(typeof students);
+let employ={
+    id:101,
+    name:"John",
+    department:"QA"
+};
+console.log(employ);
+let id1=Symbol("Employ");
+let id2=Symbol("Employ");
+console.log(id1===id2);
