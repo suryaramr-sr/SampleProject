cc69417820823657d90a60a819d2e0f04ccf202a
Surya <suryaram.r@gmail.com>
2026-07-31 02:55:21 +0530

    Fourth commit

---
+++ Basic4.js
@@ -0,0 +1,22 @@
+function greet(){
+    console.log("Welcome to Javascript");
+}
+greet();
+
+function greet1(name) {
+    console.log(name);
+}
+greet1("Anju");
+
+function add(a,b){
+    return a+b;
+    }
+ let result=add(10,20);
+ console.log(result);
+
+ function square(number){
+    return number*number;
+ }
+ console.log(square(10));
+
+ f
