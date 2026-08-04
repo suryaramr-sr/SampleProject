3f3a4e5a219e682126f44ff4852ae92a2baa4a84
Surya <suryaram.r@gmail.com>
2026-08-04 20:21:07 +0530

    11th commit

---
+++ Basic11.js
@@ -0,0 +1,9 @@
+//filter array method
+let numbers=[1,2,3,4,5,6];
+let result=numbers.filter(num=>num%2==0);
+console.log(result);
+
+let marks=[35,45,50,70,80];
+let passedStudents=marks.filter(mark=>mark>50);
+console.log(passedStudents);
+
