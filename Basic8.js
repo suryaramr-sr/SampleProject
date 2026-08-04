3047139adb7c46ca24a255c9337a64bb11e6fa03
Surya <suryaram.r@gmail.com>
2026-08-04 20:18:06 +0530

    eighth commit

---
+++ Basic8.js
@@ -0,0 +1,21 @@
+//for join 2 arrays
+let boys=["Ajay","Vinil"];
+let girls=["Anu","Anju"];
+let students=boys.concat(girls);
+console.log(students);
+
+let numbers=[10,20,30,40,50];
+//returns portion of an array
+let result=numbers.slice(1,5);
+console.log(result);
+let fruits=["Apple","Orange"];
+fruits.reverse();
+//reverse an array
+console.log(fruits);
+//for sorting
+fruits.sort();
+console.log(fruits);
+//for remove elements
+let numbers1=[10,20,30,40,50];
+numbers1.splice(1,2);
+console.log(numbers1);
