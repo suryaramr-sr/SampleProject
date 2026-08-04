a30105906ddce7c8a75626b22fe54c9e7dfd9b77
Surya <suryaram.r@gmail.com>
2026-08-04 20:18:49 +0530

    nineth commit

---
+++ Basic9.js
@@ -0,0 +1,12 @@
+//foreach method
+//syntax
+/*Array.forEach((element,index)=>{
+})*/
+const numbers=[10,20,30,40];
+numbers.forEach((num)=>{
+    console.log(num);
+})
+const fruits=["Apple,"]
+fruits.forEach((fruit,index)=>{
+    console.log(index+""+fruit);
+})
