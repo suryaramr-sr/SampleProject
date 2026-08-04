62287eb83b6826c37e0e01d47131f33b7449af25
Surya <suryaram.r@gmail.com>
2026-08-04 20:11:04 +0530

    sixth commit

---
+++ Basic6.js
@@ -0,0 +1,17 @@
+const welcome=()=>{
+    console.log("Welcome");
+}
+welcome();
+
+const square=num=>{
+    return num*num;
+}
+console.log(square(5));
+
+const add=(a,b)=>{
+    return a+b;
+}
+console.log(add(1,2));
+
+const add1=(a,b)=>a+b;
+console.log(add1(1,2));
